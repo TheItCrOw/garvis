3aff883c2228fa0035ab6fbf7e6b65a8aaa2df6e
BwandoWando <leandro.losaria@gmail.com>
2026-02-11 22:13:56 +0800

    Checking-in optional passing of binary 64 string image

--- garvis-backend/app/services/agentic_assistant_service.py
+++ garvis-backend/app/services/agentic_assistant_service.py
@@ -21,6 +21,7 @@ from langchain_openai import ChatOpenAI
 from langchain.tools import InjectedState  # per docs
 from threading import Lock
 from typing import ClassVar, Optional, Annotated
+from fastapi import HTTPException, status
 
 
 #==================
@@ -296,9 +297,9 @@ class AgenticAssistantService:
         , image_path: Optional[str] = None
         , thread_id: str = "demo"
         , display_tool_call: bool = True
+        , image_b64: Optional[str] = None
     ) -> str:
         
-        image_b64 = None
         image_mime = None
         cfg = {"configurable": {"thread_id": thread_id}}
 
@@ -306,7 +307,19 @@ class AgenticAssistantService:
         if(user_text.strip()):
             blocks.append({"type": "text", "text": user_text.strip()})
 
-        if image_path:
+        if(image_b64):
+            validation_check = image_utils.detect_image_mime_pillow(image_b64)
+
+            if(not validation_check["is_image"]):
+                raise HTTPException(
+                    status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                    detail="base64 string is not an image, incomplete, or corrupted",
+                )
+            else:
+                image_mime = validation_check["mime"]
+                blocks.append({"type": "image_url", "image_url": {"url": f"data:{image_mime};base64,{image_b64}"}})
+
+        if(image_path):
             image_mime, _ = mimetypes.guess_type(image_path)
 
             with open(image_path, "rb") as f:
@@ -362,6 +375,7 @@ class AgenticAssistantService:
             image_path=garvis_task.uploaded_file_path,
             thread_id=garvis_task.session_id,
             display_tool_call=True,
+            image_b64=garvis_task.base64_image
         )
 
         return GarvisReply(
