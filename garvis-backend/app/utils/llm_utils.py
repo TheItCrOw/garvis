84e44ca935f1b3d673cb120f82f1d0976f3ffa7e
BwandoWando <leandro.losaria@gmail.com>
2026-02-21 09:55:20 +0800

    Changes. im getting confused, i swear i just pushed these, but im doing it again

--- garvis-backend/app/utils/llm_utils.py
+++ garvis-backend/app/utils/llm_utils.py
@@ -1,6 +1,7 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_ollama import ChatOllama
 from langchain_openai import ChatOpenAI
+import os
 
 
 def instantiate_google_llm(model_name, temperature=0, max_retries=2, timeout=60):
@@ -8,18 +9,23 @@ def instantiate_google_llm(model_name, temperature=0, max_retries=2, timeout=60)
                     model=model_name,
                     temperature=temperature,
                     max_retries=max_retries,
-                    timeout = timeout)
+                    timeout = timeout,
+                    )
 
 def instantiate_ollama_llm(model_name, temperature=0, max_retries=2, timeout=60):
+    base_url = os.getenv("OLLAMA_BASE_URL", "http://localhost:11434")
     return ChatOllama(
                     model=model_name,
                     temperature=temperature,
                     max_retries=max_retries,
-                    timeout = timeout)
+                    timeout = timeout,
+                    base_url=base_url,
+                    )
 
 def instantiate_openai_llm(model_name, temperature=0, max_retries=2, timeout=60):
     return ChatOpenAI(
                     model=model_name,
                     temperature=temperature,
                     max_retries=max_retries,
-                    timeout = timeout)
+                    timeout = timeout,
+                    )
